4f7b461d87c8763ff0e027c495cdc68e45c46c5f
MS269 <ehddnr9797@gmail.com>
2024-03-04 20:23:01 +0900

    fix: bg-background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default async function Header() {
   const { session } = await validateRequest();
 
   return (
-    <div className="fixed top-0 inset-x-0 z-10 py-2 border-b">
+    <div className="fixed top-0 inset-x-0 z-10 py-2 border-b bg-background">
       <div className="max-w-sm container h-full flex items-center justify-between gap-2">
         <ModeToggle />
         <SignOutButton session={session} variant="outline" />
